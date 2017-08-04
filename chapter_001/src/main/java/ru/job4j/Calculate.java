f88ab8d206838bf6cd904bed60bec38626cd305c
Fedor Bochkarev <fbochkarev@bk.ru>
2017-08-04 11:03:04 +0200

    4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,3 +1,9 @@
+/**
+* Class Класс для вычисления арифметических операций + - * / ^.
+* @author Fedor Bochkarev (mailto:fbochkarev@bk.ru)
+* @since 04.08.2017
+* @version 1
+*/
 package ru.job4j;
 
 public class Calculate {
